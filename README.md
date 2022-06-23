f0dfdf2f6eadedb36d51c931b98e6ac4b6b64591
王一帆 <wang.yifan4@iwhalecloud.com>
2022-06-23 09:44:02 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# Obsidian File Mover
+
+This is an obsidian plugin for moving search result to target folder。
+
+# Userage
+
+- Query files from Search Plugin
+- Copy Search Result
+- Run Command 「Move files to ...」
+- Select target folder
+- Comfirm
+
+
---
+++ main.ts
@@ -0,0 +1,27 @@
+import {App, Plugin} from 'obsidian';
+import { MoveModal } from 'modal';
+
+export default class MyPlugin extends Plugin {
+
+	async onload() {
+		// This creates an icon in the left ribbon.
+		const ribbonIconEl = this.addRibbonIcon('clipboard-list', 'Move files to ...', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			new MoveModal(this.app).open();
+		});
+		// Perform additional things with the ribbon
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		// This adds a simple command that can be triggered anywhere
+		this.addCommand({
+			id: 'move-files-to',
+			name: 'Move files to ...',
+			callback: () => {
+				new MoveModal(this.app).open();
+			}
+		});
+	}
+
+	onunload() {
+	}
+}
